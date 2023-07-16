7c212d4e3df6042d8956ed6802ac134bbddeb94d
quentingruber <quentingruber@gmail.com>
2023-07-16 17:55:47 +0200

    comment nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,28 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function map(mode, lhs, rhs, opts)
+  opts = opts or {}
+  opts.silent = opts.silent ~= false
+  if opts.remap and not vim.g.vscode then
+    opts.remap = nil
+  end
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+map("n", "<leader>/", "<cmd>vsplit<cr>", { desc = "Comment" })
+map("n", "|", "<cmd>vsplit<cr>", { desc = "Vsplit" })
+map("n", "-", "<cmd>split<cr>", { desc = "Hsplit" })
+
+-- TODO: leader / not // but rn it's random if the key is set or not :(
+map("n", "<leader>//", function()
+  require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
+end, { desc = "Toggle comment line" })
+
+map(
+  "v",
+  "<leader>/",
+  "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+  { desc = "Toggle comment lines" }
+)
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,5 +1,14 @@
 return {
-  
-    { import = "lazyvim.plugins.extras.coding.yanky" },
-  
+  { import = "lazyvim.plugins.extras.coding.yanky" },
+  {
+    "numToStr/Comment.nvim",
+    keys = {
+      { "gc", mode = { "n", "v" }, desc = "Comment toggle linewise" },
+      { "gb", mode = { "n", "v" }, desc = "Comment toggle blockwise" },
+    },
+    opts = function()
+      local commentstring_avail, commentstring = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
+      return commentstring_avail and commentstring and { pre_hook = commentstring.create_pre_hook() } or {}
+    end,
+  },
 }
