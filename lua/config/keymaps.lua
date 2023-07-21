0e3a11edaa4fcdd57b6e56d4861a62f9c4a4e976
quentingruber <quentingruber@gmail.com>
2023-07-21 19:09:21 +0200

    fix keymap comment and add temp node term

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,10 +11,9 @@ map("n", "<leader>/", "<cmd>vsplit<cr>", { desc = "Comment" })
 map("n", "|", "<cmd>vsplit<cr>", { desc = "Vsplit" })
 map("n", "-", "<cmd>split<cr>", { desc = "Hsplit" })
 
--- TODO: leader / not // but rn it's random if the key is set or not :(
-map("n", "<leader>//", function()
+map("n", "<leader>/", function()
   require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
-end, { desc = "Toggle comment line" })
+end, { desc = "Toggle comment line", remap = true })
 
 map(
   "v",
@@ -23,15 +22,22 @@ map(
   { desc = "Toggle comment lines" }
 )
 
--- TODO: mutiple terminals
--- TODO: pass a defaukt command to toggle_term_cmd example "nu"
-map("n", "<leader>tt", "<cmd>ToggleTerm nu<cr>", { desc = "Toggle terminal" })
--- if vim.fn.executable "node" == 1 then
---   map("n", "<leader>tn", "<cmd>:ToggleTerm node<cr>", { desc = "ToggleTerm node" })
--- end
--- local python = vim.fn.executable "python" == 1 and "python" or vim.fn.executable "python3" == 1 and "python3"
--- if python then map("t", "<leader>tp", "<cmd>:ToggleTerm python<cr>", { desc = "ToggleTerm python" }) end
-map("n", "<leader>tf", "<cmd>:ToggleTerm direction=float nu<cr>", { desc = "ToggleTerm float" })
-map("n", "<leader>th", "<cmd>:ToggleTerm size=10 direction=horizontal nu<cr>",
+
+-- Terminals
+
+-- Base terminal
+map("n", "<leader>tt", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
+map("n", "<leader>tf", "<cmd>:ToggleTerm direction=float<cr>", { desc = "ToggleTerm float" })
+map("n", "<leader>th", "<cmd>:ToggleTerm size=10 direction=horizontal<cr>",
   { desc = "ToggleTerm horizontal split" })
-map("n", "<leader>tv", "<cmd>:ToggleTerm size=80 direction=vertical nu<cr>", { desc = "ToggleTerm vertical split" })
+map("n", "<leader>tv", "<cmd>:ToggleTerm size=80 direction=vertical<cr>", { desc = "ToggleTerm vertical split" })
+
+
+-- Node
+local function createNodeTerm()
+  require("toggleterm.terminal").Terminal:new({ cmd = "node", hidden = true }):toggle();
+end
+if vim.fn.executable "node" == 1 then
+  map("n", "<leader>tn", function() createNodeTerm() end, { desc = "ToggleTerm node" })
+end
+
