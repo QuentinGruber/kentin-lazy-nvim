6ebd2594f0b29d94abebeb2c01764e93f3248171
Qgruber <47059878+QuentinGruber@users.noreply.github.com>
2023-09-29 14:56:31 +0200

    Add files via upload

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,15 @@
 return {
   { "navarasu/onedark.nvim" },
   { "EdenEast/nightfox.nvim" },
+  { "sainnhe/sonokai" },
+  -- andromeda
+  { "tjdevries/colorbuddy.nvim"},
+  {"nobbmaestro/nvim-andromeda"},
+  -- end andromeda 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfox",
+      colorscheme = "sonokai",
     },
   },
 }
