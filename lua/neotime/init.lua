c00dbd797431f054ec5c8a06953629295856cacc
quentingruber <quentingruber@gmail.com>
2024-07-02 14:29:33 +0200

    timewasted :D

--- lua/neotime/init.lua
+++ lua/neotime/init.lua
@@ -1,7 +1,14 @@
-local count = 0
+local startTime = os.time()
 local function getStatusLine()
-  count = count + 1
-  return count
+  local actual = os.time()
+  local diff = os.difftime(actual, startTime)
+  local suffix = " seconds"
+  local timeToDisplay = diff
+  if diff > 60 then
+    timeToDisplay = timeToDisplay / 60
+    suffix = " minutes"
+  end
+  return "Time wasted : " .. timeToDisplay .. suffix
 end
 
 return {
