3ad26fef23c7404d2956c3244f865a03203028e4
quentingruber <quentingruber@gmail.com>
2024-07-02 14:16:09 +0200

    add hardtimeconfig

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,14 @@
+return {}
+-- return {
+--   {
+--     "m4xshen/hardtime.nvim",
+--     event = "VeryLazy",
+--     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+--     opts = {},
+--   },
+--   {
+--     "tris203/precognition.nvim",
+--     event = "VeryLazy",
+--     opts = {},
+--   },
+-- }
