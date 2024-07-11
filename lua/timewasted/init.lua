c3452285bb3de3ff4c2272284384bbad03e69539
quentingruber <quentingruber@gmail.com>
2024-07-11 13:56:43 +0200

    fix bug in timewasted

--- lua/timewasted/init.lua
+++ lua/timewasted/init.lua
@@ -2,8 +2,8 @@ local startTime = os.time()
 local function getStatusLine()
   local actual = os.time()
   local diff = os.difftime(actual, startTime)
-  local hours = diff / 3600
-  local minutes = diff / 60
+  local hours = diff / 3600 % 3600
+  local minutes = diff / 60 % 60
   local seconds = diff % 60
   local timeString = string.format("%02d:%02d:%02d", hours, minutes, seconds)
 
