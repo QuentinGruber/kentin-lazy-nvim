f3c005caa480ea8dc3939dd36c3a111789dba81e
quentingruber <quentingruber@gmail.com>
2024-07-26 00:50:05 +0200

    optsss

--- lua/plugins/gh.lua
+++ lua/plugins/gh.lua
@@ -4,10 +4,8 @@ return {
 
   {
     "pwntester/octo.nvim",
-    requires = { "nvim-lua/plenary.nvim","nvim-tree/nvim-web-devicons" },
-    config = function()
-      require "octo".setup()
-    end
-  }
+    requires = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+    opts = {},
+  },
   -- end of octo
 }
