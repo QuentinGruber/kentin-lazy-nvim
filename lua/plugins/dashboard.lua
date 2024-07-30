3265b1ebd9c3621f6d1ea31520f78d2ce8e8e9d8
quentingruber <quentingruber@gmail.com>
2024-07-30 21:07:03 +0200

    use hyper theme

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -77,8 +77,26 @@ return {
       math.randomseed(os.time())
       local random_logo = logos[math.random(1, #logos)]
 
+      local shortcut = {
+        { icon = "  ", desc = "Find file", action = "Telescope find_files", key = "f" },
+        { icon = "  ", desc = "Recent files", action = "Telescope oldfiles", key = "r" },
+        { icon = "  ", desc = "Find text", action = ":Telescope live_grep", key = "g" },
+        { icon = "  ", desc = "Config", action = "e $MYVIMRC", key = "c" },
+        { icon = "  ", desc = "Mason", action = "Mason", key = "m" },
+        { icon = "  ", desc = "Restore Session", action = "lua require('persistence').load()", key = "s" },
+        { icon = "󰒲  ", desc = "Lazy", action = ":Lazy", key = "l" },
+        {
+          action = function()
+            vim.api.nvim_input("<cmd>qa<cr>")
+          end,
+          desc = " Quit",
+          icon = " ",
+          key = "q",
+        },
+      }
+
       local opts = {
-        theme = "doom",
+        theme = "hyper",
         hide = {
           -- this is taken care of by lualine
           -- enabling this messes up the actual laststatus setting after loading a file
@@ -86,34 +104,12 @@ return {
         },
         config = {
           header = random_logo,
-          center = {
-            { icon = "  ", desc = "Find file", action = "Telescope find_files", key = "f" },
-            { icon = "  ", desc = "Recent files", action = "Telescope oldfiles", key = "r" },
-            { icon = "  ", desc = "Find text", action = ":Telescope live_grep", key = "g" },
-            { icon = "  ", desc = "Config", action = "e $MYVIMRC", key = "c" },
-            { icon = "  ", desc = "Mason", action = "Mason", key = "m" },
-            { icon = "  ", desc = "Restore Session", action = "lua require('persistence').load()", key = "s" },
-            { icon = "󰒲  ", desc = "Lazy", action = ":Lazy", key = "l" },
-            {
-              action = function()
-                vim.api.nvim_input("<cmd>qa<cr>")
-              end,
-              desc = " Quit",
-              icon = " ",
-              key = "q",
-            },
-          },
+          shortcut = shortcut,
           footer = function()
-            local stats = require("lazy").stats()
-            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+            return {}
           end,
         },
       }
-      for _, button in ipairs(opts.config.center) do
-        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-        button.key_format = "  %s"
-      end
 
       -- open dashboard after closing lazy
       if vim.o.filetype == "lazy" then
