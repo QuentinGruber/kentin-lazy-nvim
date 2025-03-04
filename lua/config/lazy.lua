f8eb53830ded2b27581a5bfadd3268b24aa16ae4
Quentin GRUBER <q.gruber@proxiad.com>
2025-03-04 11:30:45 +0100

    updt lazynvim + vscode support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,7 +10,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", tag = "v12.44.1", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
   },
